97e516e9277fd0f04c60853b1a0552b22f5e4e3a
santiagoescarate-coder <santiago.escarate@gmail.com>
2026-08-17 14:55:00 -0400

    Corrige rutas de caché PWA

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'finanzas-sencillas-v26-1';
+const CACHE_NAME = 'finanzas-sencillas-v26-2';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
