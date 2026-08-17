5e21e7fd0d02a8dca2d8bd854daf51539dd1c6ad
santiagoescarate-coder <santiago.escarate@gmail.com>
2026-08-17 14:32:13 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,7 +26,12 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+const requestUrl = new URL(event.request.url);
 
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+  
   event.respondWith(
     fetch(event.request)
       .then(response => {
